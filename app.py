127b338204c051988ae35e8f9c950d9d8b66af59
koumikads <147312314+koumikads@users.noreply.github.com>
2023-10-12 09:24:16 -0700

    Update app.py
    push

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, jsonify, request
 import cv2
 import torch
 import torchvision
+
 app = Flask(__name__)
 
-
 # Load the pre-trained YOLOv5 model
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='path/to/your/best.pt')
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
 
 # Start capturing frames from the webcam
 cap = cv2.VideoCapture(0)
